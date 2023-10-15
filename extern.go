cd67ba8355f1ffd3664e13a9f0247f44dc4c81d4
Kiswono Prayogo <kiswono@gmail.com>
2023-10-15 07:42:10 +0700

    Update extern -I -L to match ubuntu's default dir

--- extern.go
+++ extern.go
@@ -6,8 +6,8 @@ import (
 	"sync"
 )
 
-// #cgo CFLAGS: -I/usr/local/include/luajit-2.1
-// #cgo LDFLAGS:  -L/usr/local/lib -lluajit -ldl -lm
+// #cgo CFLAGS: -I/usr/include/luajit-2.1
+// #cgo LDFLAGS:  -L/usr/lib/x86_64-linux-gnu -lluajit-5.1 -ldl -lm
 //#include "glua.h"
 import "C"
 
